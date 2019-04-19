f99b6b52a92b93ed82d25135e30462f69558017e
unosk <unosk.s@gmail.com>
2019-04-19 17:04:57 +0900

    License表示コンポーネントを追加

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,6 +1,22 @@
 declare module 'react-native-oss-licenses' {
+  import React from 'react'
+  import { ViewStyle, StyleProp } from 'react-native'
+
   export interface License {
     project: string
     license: string
   }
+
+  export interface LicenseListProps {
+    data: License[]
+    style?: StyleProp<ViewStyle>
+  }
+
+  export class LicenseList extends React.Component<LicenseListProps> {}
+
+  export interface LicenseItemProps {
+    license: License
+  }
+
+  export class LicenseItem extends React.Component<LicenseItemProps> {}
 }
